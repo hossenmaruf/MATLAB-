37bb04038cfd632d4cac659bc5effdfc999ea483
hossenmaruf <severusmaruf740@gmail.com>
2023-09-15 05:02:55 +0600

    3D plot

--- plot3d.m
+++ plot3d.m
@@ -6,13 +6,23 @@ clear all ;
 
 subplot(2,2,1)
 surf(x , y, z)
-
+colormap(cool)
+xlabel ('X Axis')
+ylabel ('Y Axis')
+zlabel ('Z Axis')
+title('Peaks')
 
 subplot(2,2,2)
-mesh(x,y,z)
+[x,y,z] = sphere
+surf(x , y, z)
 
 subplot(2,2,3)
-contour(x,y,z)
+[x,y,z] = cylinder
+surf(x , y, z)
 
 subplot(2,2,4)
-pcolor(x,y,z)
+x = 1:3
+y = 1:5
+[X Y] = meshgrid(x,y)
+f = X.^2 + Y.^4
+surf(X ,Y, f)
